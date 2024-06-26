d9798d1ff89fa5556763358999de04abae74faa2
Milana32 <157360971+Milana32@users.noreply.github.com>
2024-06-26 13:25:21 +0300

    Реализация main.cpp

--- ConvertingInequalityToOperationLess/ConvertingInequalityToOperationLess/main.cpp
+++ ConvertingInequalityToOperationLess/ConvertingInequalityToOperationLess/main.cpp
@@ -43,11 +43,113 @@
  * \param [in] argv[2] Путь к выходному файлу (.txt) для сохранения результата.
  * \return 0, если программа завершилась успешно, -1 в случае ошибки.
  */
-
 int main(int argc, char *argv[])
 {
     runTests();
     QCoreApplication a(argc, argv);
 
-    return a.exec();
+    // Включить поддержку русских символов в консоли
+    setlocale(LC_ALL, "Russian");
+
+    QList<Error> errors;
+    QString filePath;
+    QString outputPath;
+
+    // Вывод запроса на ввод пути к файлу
+    std::cout << "Input path to file (.txt): ";
+    std::flush(std::cout); // Сбрасываем буфер вывода, чтобы сообщение появилось сразу
+
+    // Считываем ввод с консоли
+    std::string tempPath;
+    std::getline(std::cin, tempPath);
+
+    // Преобразуем std::string в QString
+    filePath = QString::fromStdString(tempPath);
+
+    // Удаляем символы перевода строки или другие пробельные символы в конце строки, если они есть
+    filePath = filePath.trimmed();
+
+    // Проверяем существование файла
+    QFile file(filePath);
+    if (!file.exists()) {
+        qDebug() << "FILE_NOT_FOUND: Файл по пути не найден";
+        return -1;
+    }
+
+    // Получение строк из файла
+    QStringList fileContent = getStringsFromFile(filePath, errors);
+
+    if (fileContent.isEmpty()) {
+        for (const Error& error : errors) {
+            qDebug() << ErrorToString(error);
+        }
+        return -1;
+    }
+
+    QString inequality = fileContent.first();
+
+    // Построение дерева логического выражения
+    TreeNode* logicalTree = buildLogicalTree(inequality, errors);
+
+    if (!logicalTree) {
+        for (const Error& error : errors) {
+            qDebug() << ErrorToString(error);
+        }
+        return -1;
+    }
+
+    QString inequality_result = treeToString(logicalTree, errors);
+
+    // Преобразование операторов сравнения
+    convertToLess(logicalTree, errors);
+
+    // Преобразование дерева в строку
+    QString result = treeToString(logicalTree, errors);
+
+    if (result.isEmpty()) {
+        for (const Error& error : errors) {
+            qDebug() << ErrorToString(error);
+        }
+        return -1;
+    }
+
+    qDebug() << "Исходное неравенство в инфиксной форме:" << inequality_result;
+    qDebug() << "Полученное неравенство в инфиксной форме:" << result;
+
+    // Вывод запроса на ввод пути к выходному файлу
+    std::cout << "Input path to output file (.txt): ";
+    std::flush(std::cout); // Сбрасываем буфер вывода, чтобы сообщение появилось сразу
+
+    // Считываем ввод с консоли
+    std::string tempOutputPath;
+    std::getline(std::cin, tempOutputPath);
+
+    // Преобразуем std::string в QString
+    outputPath = QString::fromStdString(tempOutputPath);
+
+    // Удаляем символы перевода строки или другие пробельные символы в конце строки, если они есть
+    outputPath = outputPath.trimmed();
+
+    // Записываем результат в выходной файл
+    QFile outputFile(outputPath);
+    if (!outputFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        qDebug() << "FILE_NOT_OPENED: Не удалось открыть файл для записи";
+        return -1;
+    }
+
+    QTextStream out(&outputFile);
+    out << result;
+    outputFile.close();
+
+    qDebug() << "Результат успешно записан в файл:" << outputPath;
+
+    // // Вывод всех найденных ошибок
+    // if (!errors.isEmpty()) {
+    //     qDebug() << "Список найденных ошибок:";
+    //     for (const Error& error : errors) {
+    //         qDebug() << ErrorToString(error);
+    //     }
+    // }
+
+    return 0;
 }
