dfe3f91cf04430e8576e9d31b034c4f70a4f4d7f
Milana32 <157360971+Milana32@users.noreply.github.com>
2024-06-26 13:04:47 +0300

    Добавление тестов для функции isOperator

--- ConvertingInequalityToOperationLess/ConvertingInequalityToOperationLess/tests.cpp
+++ ConvertingInequalityToOperationLess/ConvertingInequalityToOperationLess/tests.cpp
@@ -16,3 +16,39 @@ void runTests()
     Tests tests;
     QTest::qExec(&tests);
 }
+
+// Тесты для функции isOperator
+void Tests::testisOperator()
+{
+    QFETCH(QString, token);
+    QFETCH(bool, expected);
+
+    QList<Error> errors;
+    bool result = isOperator(token, errors);
+    QCOMPARE(result, expected);
+}
+
+void Tests::testisOperator_data()
+{
+    QTest::addColumn<QString>("token");
+    QTest::addColumn<bool>("expected");
+
+    QTest::newRow("plus") << "+" << true;
+    QTest::newRow("minus") << "-" << true;
+    QTest::newRow("multiply") << "*" << true;
+    QTest::newRow("division") << "/" << true;
+    QTest::newRow("remainder of division") << "%" << true;
+    QTest::newRow("greater than") << ">" << true;
+    QTest::newRow("greater_or_equal") << ">=" << true;
+    QTest::newRow("less_than") << "<" << true;
+    QTest::newRow("less_or_equal") << "<=" << true;
+    QTest::newRow("negation") << "!" << true;
+    QTest::newRow("unary_plus") << "_-" << true;
+    QTest::newRow("unary_minus") << "_+" << true;
+    QTest::newRow("empty_row") << " " << false;
+    QTest::newRow("space_operator") << " > " << false;
+    QTest::newRow("unknown_character") << "&" << false;
+    QTest::newRow("unknown_character") << "++" << false;
+    QTest::newRow("unknown_character") << "--" << false;
+    QTest::newRow("unknown_character") << "**" << false;
+}
