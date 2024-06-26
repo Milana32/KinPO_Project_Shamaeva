2790a58c23c5478474a4ad1aad7e92272154ecde
Milana32 <157360971+Milana32@users.noreply.github.com>
2024-06-26 13:06:08 +0300

    Добавила тесты для функции isValidOperand

--- ConvertingInequalityToOperationLess/ConvertingInequalityToOperationLess/tests.cpp
+++ ConvertingInequalityToOperationLess/ConvertingInequalityToOperationLess/tests.cpp
@@ -52,3 +52,43 @@ void Tests::testisOperator_data()
     QTest::newRow("unknown_character") << "--" << false;
     QTest::newRow("unknown_character") << "**" << false;
 }
+
+// Тесты для функции isValidOperand
+void Tests::testisValidOperand()
+{
+    QFETCH(QString, token);
+    QFETCH(bool, expected);
+
+    QList<Error> errors;
+    bool result = isValidOperand(token, errors);
+    QCOMPARE(result, expected);
+}
+
+void Tests::testisValidOperand_data()
+{
+
+    QTest::addColumn<QString>("token");
+    QTest::addColumn<bool>("expected");
+
+    QTest::newRow("valid_operator") << "123" << true;
+    QTest::newRow("valid_negative_number") << "-123" << true;
+    QTest::newRow("minimum_number") << "-214747483647" << true;
+    QTest::newRow("maximum_number") << "214747483647" << true;
+    QTest::newRow("number_out_of_range") << "214747483648" << false;
+    QTest::newRow("negative_number_out_of_range") << "-214747483648" << false;
+    QTest::newRow("variable_name_with_latin_letters") << "varName" << true;
+    QTest::newRow("valid_variable_with_number") << "var123" << true;
+    QTest::newRow("variable_name_with_underscore") << "var_name" << true;
+    QTest::newRow("invalid_variable_starting_with_number") << "1var" << false;
+    QTest::newRow("variable_name_with_a_length_of_20_characters") << "variableName20Chars" << true;
+    QTest::newRow("variable_name_longer_than_20_characters") << "variableNameIsTooLong" << false;
+    QTest::newRow("empty_string") << QString("") << false;
+    QTest::newRow("invalid_variable_with_special_char") << "var@123" << false;
+    QTest::newRow("string_with_spaces") << "var name" << false;
+    QTest::newRow("string_with_only_numbers") << "1234567890" << true;
+    QTest::newRow("string_with_special_characters") << "var$name" << false;
+    QTest::newRow("string_with_uppercase") << "VARNAME" << true;
+    QTest::newRow("string_in_mixed_case") << "VarName" << true;
+}
+
+
